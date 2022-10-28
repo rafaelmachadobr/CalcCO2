6a37cc55a3ce38c1fa6052a380e7913fba0dd150
Rafael <rafaelf.machado2004@gmail.com>
2022-10-28 11:06:00 -0300

    Adicionando novos combustíveis

--- CalcCO2.py
+++ CalcCO2.py
@@ -1,151 +1,123 @@
-total_carbono = 0
+from math import ceil
 
-print('-' * 45)
-print('Descubra se você taxista ou motorista de aplicativo possui crédito de CO²')
-print('-' * 45)
-print("Primeiramente, vamos precisar de alguns dados!\n")
 
-gasto_combustivel = -1
+alcool = 5 * 0.82 * 0.79 * 3.7
+diesel = 5 * 0.83 * 3.7
+etanol = 5 * 0.82 * 0.78 * 3.7
+gasolina = 5 * 0.82 * 0.75 * 3.7
+continuar = 's'
 
-while gasto_combustivel <= -1:
+print('-' * 80)
+print('Descubra se você taxista ou motorista de aplicativo possui ou deve crédito de CO²')
+print('-' * 80)
+print("Primeiramente, vamos precisar de alguns dados!")
+
+while continuar != 'n':
     try:
-        gasto_combustivel = float(
-            input('Digite a média mensal de gasto seu com combustível: R$').replace(',', '.'))
-        if gasto_combustivel < 0:
-            print('-' * 45)
-            print('Não pode ser numero negativo!')
-            print('-' * 45)
-    except ValueError:
-        print('-' * 45)
-        print('Isso não é um número válido. Tente novamente')
-        print('-' * 45)
+        distancia_dia = int(
+            input('\nDigite a média de quilômetro (Km) que seu carro roda por dia: '))
 
-tipo_combustivel = ''
+        taxa_consumo = int(
+            input('Digite a média de quilômetro por litro (Km/L) do seu carro: '))
 
-while not tipo_combustivel == '1' and not tipo_combustivel == '2' and not tipo_combustivel == '3':
-    tipo_combustivel = input(
-        f"Escolha o tipo de combustível que você utiliza:\n"
-        "[ 1 ] - Gasolina \n"
-        "[ 2 ] - Etanol \n"
-        "[ 3 ] - Diesel \n"
-        "Digite sua opção: ").replace(' ', '')
-
-    if tipo_combustivel == '1':
-        combustivel = 0.0035
-    elif tipo_combustivel == '2':
-        combustivel = 0.0039
-    elif tipo_combustivel == '3':
-        combustivel = 0.0042
-    else:
-        print('-' * 45)
-        print(f'"{tipo_combustivel}" não é um opção valida, tente novamente!')
-        print('-' * 45)
-
-total_combustivel = gasto_combustivel * combustivel
-total_carbono += total_combustivel
-
-print(
-    f"\nVocê está utilização de combustiveis {total_combustivel:.2f} toneladas de CO² por ano, \n"
-    f"Até o momento você polui {total_carbono:.2f} toneladas de CO² por ano!\n")
-
-conta_luz = -1
-
-while conta_luz < 0:
-    try:
-        conta_luz = float(
-            input("Digite o valor médio da conta de luz da sua empresa: R$"))
-
-        if conta_luz < 0:
-            print('-' * 45)
-            print('Não pode ser numero negativo!')
-            print('-' * 45)
-        else:
-            luz = conta_luz * 0.002136
-            total_carbono += luz
-
-            print(f"Sua empresa emite {luz:.2f} toneladas de CO² por ano,\ntotalizando até agora "
-                  f"{total_carbono:.2f} toneladas de CO² por ano!\n")
-
-    except ValueError:
-        print('-' * 45)
-        print('Isso não é um número válido. Tente novamente')
-        print('-' * 45)
-
-frete_mes = -1
-
-while frete_mes < 0:
-    try:
-        frete_mes = int(
-            input('Digite a quantidade de viagens que você faz ao mês: '))
-
-        if frete_mes < 0:
-            print('-' * 45)
-            print('Não pode ser numero negativo! Tente novamente')
-            print('-' * 45)
+        if taxa_consumo <= 0:
+            print(f'\n"{taxa_consumo}" não pode, tente novamente!')
             continue
 
-    except ValueError:
-        print('-' * 45)
-        print('Isso não é um número válido. Tente novamente')
-        print('-' * 45)
+        consumo_diario = distancia_dia / taxa_consumo
+        consumo_anual = consumo_diario * 53
 
-distancia = ''
+        print(f"\nSeu consumo diário é de {consumo_diario:.2f} Litros,\n"
+              f"Já o seu consumo anual é de {consumo_anual:.2f} Litros\n")
 
-while not distancia == '1' and not distancia == '2' and not distancia == '3':
-    distancia = input(f"Escolha a localização da maioria das viagens:\n"
-                      "[ 1 ] - 1 km até 100 km \n"
-                      "[ 2 ] - 100 km a 600 km \n"
-                      "[ 3 ] - 600 km até 2000 km \n"
-                      "Digite sua opção: ").replace(' ', '')
+        tipo_combustivel = input(
+            f"Escolha o tipo de combustível que você utiliza:\n"
+            "[ 1 ] - Álcool \n"
+            "[ 2 ] - Diesel \n"
+            "[ 3 ] - Etanol \n"
+            "[ 4 ] - Gasolina \n"
+            "Digite sua opção: ").replace(' ', '').lower()
 
-    if distancia == '1':
-        media_frete = frete_mes * 0.00213
-    elif distancia == '2':
-        media_frete = frete_mes * 0.00381
-    elif distancia == '3':
-        media_frete = frete_mes * 0.00416
-    else:
-        print('-' * 45)
-        print(f'"{distancia}" não é um opção valida, tente novamente!')
-        print('-' * 45)
-
-    total_carbono += media_frete
-
-
-print(f"Você emite por meio de utilização de viagens {media_frete:.2f} toneladas de CO² por ano,\ntotalizando até agora "
-    f"{total_carbono:.2f} toneladas de CO² por ano!\n")
-
-arvores_plantadas = int(input('Quantas árvores sua empresa planta por ano?: '))
-meta_arvores = round(total_carbono * 7)
-credito = 0
-
-if arvores_plantadas > meta_arvores:
-    for i in range(meta_arvores, arvores_plantadas):
-        credito += 1
-    print(f"Você plantou {arvores_plantadas} arvores e precisava plantar {meta_arvores} arvores para compensar o carbono emitido pela sua empresa, então recebe {credito} créditos de carbono por isso!")
-
-    vender_credito = ''
-
-    while not vender_credito == '1' and not vender_credito == '2': 
-        vender_credito = input(f"Deseja converter o(s) {credito} crédito(s) de carbono em R$:\n"
-                                "[ 1 ] - Sim\n"
-                                "[ 2 ] - Não\n" 
-                                "Digite sua opção: ").replace(' ', '')
-        if vender_credito == '1':
-            credito_convertido = f'R${credito * 365:_.2f}'
-            credito_convertido = credito_convertido.replace('.', ',').replace('_', '.')
-            print(f'Convertendo para real você terá: {credito_convertido} disponíveis.')
-        elif vender_credito == '2':
-            pass
+        if tipo_combustivel == '1' or tipo_combustivel == 'alcool':
+            emissoes_totais = consumo_anual * alcool
+        elif tipo_combustivel == '2' or tipo_combustivel == 'diesel':
+            emissoes_totais = consumo_anual * diesel
+        elif tipo_combustivel == '3' or tipo_combustivel == 'etanol':
+            emissoes_totais = consumo_anual * etanol
+        elif tipo_combustivel == '4' or tipo_combustivel == 'gasolina':
+            emissoes_totais = consumo_anual * gasolina
         else:
-            print(f'"{vender_credito}" não é um opção valida, tente novamente!')
+            print(f'"{tipo_combustivel}" não é um opção valida, tente novamente!\n')
+            continue
 
-elif arvores_plantadas == meta_arvores:
-    print(
-        f'Você consiguiu chegar a meta de {meta_arvores} arvores da sua meta para compensar o carbono emitido pela empresa.')
-else:
-    print(
-        f'Você não consiguiu bater a meta de {meta_arvores} arvores para compensar o carbono emitido pela empresa.\n'
-        f'Você prescisa comprar {meta_arvores - arvores_plantadas} credito(s) de carbono')
+        print(
+            f"\nSua emissão total é de {emissoes_totais:.2f} kg de CO2 por ano.\n")
 
-print('Fim do Programa!')
+        numero_pessoas = int(
+            input("Digite a média de passageiros que você pega por dia: "))
+
+        emisao_pessoa = emissoes_totais / numero_pessoas
+
+        print(
+            f"\nA emissão por pessoa fica assim {emisao_pessoa:.2f} kg de CO2 por ano.\n")
+
+        plantar_arvores_atlantica = ceil(emisao_pessoa / 130)
+        plantar_arvores_amazonia = ceil(emisao_pessoa / 220)
+
+        plantou = input('Você plantou alguma arvore ano passado? [s/n] ')
+        if plantou == 's' or plantou == 'sim':
+            arvores_plantadas = int(
+                input('Quantas árvores você plantou no ano passado: '))
+        else:
+            arvores_plantadas = 0
+
+        credito = (arvores_plantadas - plantar_arvores_atlantica) / 7
+        if arvores_plantadas > plantar_arvores_amazonia and arvores_plantadas > plantar_arvores_atlantica:
+            print(
+                f'\nParabéns você passou da meta que era de {plantar_arvores_atlantica} arvores,\n'
+                f'Por isso você ganha {credito:.2f} credito(s) de carbono.\n')
+
+            vender_credito = input(f"Deseja converter o(s) {credito:.2f} crédito(s) de carbono em R$:\n"
+                                   "[ 1 ] - Sim\n"
+                                   "[ 2 ] - Não\n"
+                                   "Digite sua opção: ").replace(' ', '')
+            if vender_credito == '1':
+                credito_convertido = f'R${credito * 365:_.2f}'
+                credito_convertido = credito_convertido.replace(
+                    '.', ',').replace('_', '.')
+                print(
+                    f'\nConvertendo para real você terá: {credito_convertido} disponíveis.\n')
+            elif vender_credito == '2':
+                pass
+            else:
+                print(
+                    f'"{vender_credito}" não é um opção valida, tente novamente!')
+                continue
+        elif arvores_plantadas == plantar_arvores_atlantica:
+            print(
+                '\nVocê está equilibrado não prescisa comprar credito de carbono ou arvore\n')
+        else:
+            if arvores_plantadas > plantar_arvores_amazonia:
+                print(
+                    f'\nPara compensar a taxa de emissão de CO², será necessário plantar mais {plantar_arvores_atlantica - arvores_plantadas} árvore(s) na Mata Atlântica.\n')
+            else:
+                credito = (plantar_arvores_atlantica) / 7
+                print(
+                    f'\nPara compensar a taxa de emissão de CO², será necessário plantar mais {plantar_arvores_atlantica - arvores_plantadas} árvore(s) na Mata Atlântica,\n'
+                    f'ou plantar mais {plantar_arvores_amazonia - arvores_plantadas} árvores na Amazônia.')
+                credito_convertido = f'R${credito * 365:_.2f}'
+                credito_convertido = credito_convertido.replace(
+                    '.', ',').replace('_', '.')
+                print(
+                    f'Ou comprar {credito:.2f} credito(s) de carbono, que convertendo em real fica {credito_convertido}.\n')
+
+        continuar = input(
+            'Deseja fazer outro calculo no programa: [s/n] ').lower()
+    except ValueError:
+        print()
+        print('-' * 40)
+        print('Ops! aconteceu um erro, tente novamente!')
+        print('-' * 40)
+
+
+print('\nFim do Programa!')
